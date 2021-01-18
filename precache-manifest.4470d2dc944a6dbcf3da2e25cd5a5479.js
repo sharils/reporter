f87245c005cd21bd03fac3c6d5b528025c47374a
Bestian Tang 小巴 <bestian@gmail.com>
2021-01-18 15:17:10 +0800

    deploy

---
+++ precache-manifest.4470d2dc944a6dbcf3da2e25cd5a5479.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e596c599da0fa9490da1",
+    "url": "/reporter/css/app.493111f8.css"
+  },
+  {
+    "revision": "e6248a6862d810433e07481a30458867",
+    "url": "/reporter/g0v.json"
+  },
+  {
+    "revision": "11d28aafbad99d9168517daaa365ae21",
+    "url": "/reporter/images/og.png"
+  },
+  {
+    "revision": "df0213a7cae0495bc8d285ee081eacf4",
+    "url": "/reporter/images/r2.png"
+  },
+  {
+    "revision": "2032f467b6751151b68334084988a213",
+    "url": "/reporter/index.html"
+  },
+  {
+    "revision": "4bd2b59c407c44f7415f",
+    "url": "/reporter/js/about.ff9c8b0a.js"
+  },
+  {
+    "revision": "e596c599da0fa9490da1",
+    "url": "/reporter/js/app.a2ef56fe.js"
+  },
+  {
+    "revision": "53f60ff48421c1d9b40a",
+    "url": "/reporter/js/chunk-vendors.90c92bdf.js"
+  },
+  {
+    "revision": "20e3b7fee58ddce319631bc3e35e987a",
+    "url": "/reporter/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/reporter/robots.txt"
+  }
+]);
